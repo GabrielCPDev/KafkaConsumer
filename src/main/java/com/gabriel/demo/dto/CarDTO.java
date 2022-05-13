1d7a2d72ad91d8ff986d0c973976d5157782d5d7
GabrielCPDev <gabrielcpcontato@outlook.com>
2022-05-13 16:21:22 -0300

    Adicionando configurações  em application.properties Criando classe de  DTO e Classe de configuração do kafka para o consumer

---
+++ src/main/java/com/gabriel/demo/dto/CarDTO.java
@@ -0,0 +1,13 @@
+package com.gabriel.demo.dto;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+public class CarDTO {
+
+    private String id;
+    private String model;
+    private String color;
+}
